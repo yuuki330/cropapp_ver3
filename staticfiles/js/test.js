260b8ead22ff8c8ce7a3dc7de11104646421fbc3
yuuki330 <katsuyu107@gmail.com>
2022-01-05 11:30:04 +0900

    ios11

---
+++ staticfiles/js/test.js
@@ -0,0 +1,22 @@
+
+const medias = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }
+  };
+  const video = document.getElementById("video");
+  const promise = navigator.mediaDevices.getUserMedia(medias);
+  
+  promise.then(successCallback)
+         .catch(errorCallback);
+  
+  function successCallback(stream) {
+    video.srcObject = stream;
+  };
+  
+  function errorCallback(err) {
+    alert(err);
+  };
