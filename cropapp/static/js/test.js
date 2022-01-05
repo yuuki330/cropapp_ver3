0246801a47b3bb2aff7d16969e02156e78cac2db
yuuki330 <katsuyu107@gmail.com>
2022-01-05 12:03:57 +0900

    canvas

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -1,17 +1,33 @@
 const medias = {
-    audio : false,
-    video : true
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }
   };
   const video = document.getElementById("video");
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
   const promise = navigator.mediaDevices.getUserMedia(medias);
   
   promise.then(successCallback)
-         .then(errorCallback);
+         .catch(errorCallback);
   
   function successCallback(stream) {
     video.srcObject = stream;
-  };
+    requestAnimationFrame(draw);
+  }
   
   function errorCallback(err) {
+    console.log(err);
     alert(err);
-  };
+  }
+  
+  function draw() {
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.drawImage(video, 0, 0);
+  
+    requestAnimationFrame(draw);
+  }
