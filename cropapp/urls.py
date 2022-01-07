24e6d3d3751fb3a4570ffdda470501d56cd2a2c3
yuuki330 <katsuyu107@gmail.com>
2022-01-07 21:11:06 +0900

    change views

--- cropapp/urls.py
+++ cropapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-import views
 from.views import IndexView, AboutView, TestView, WebcamView, HomeView
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', HomeView.home, name='main'),
     path('about/', AboutView.as_view()),
 ]
--- cropapp/views.py
+++ cropapp/views.py
@@ -17,21 +17,21 @@ class WebcamView(TemplateView):
 
 class HomeView(TemplateView):
     template_name = "cropapp/home.html"
+    def home(request):
+        # context = {
+        #     'tokyo_today_w':'晴れ'
+        # }
+        url = 'https://weather.yahoo.co.jp/weather/jp/3.html?day=1' #気象庁のHP
+        res = requests.get(url)
+        res.encoding = res.apparent_encoding
+        soup = BeautifulSoup(res.text, "html.parser")
+        weathers = soup.find_all(class_='weather')
+        date_list = weather_list = []
+        for i  in weathers:
+            weather = re.findall('alt="(.*?)" src', str(i))
+            weather_list = weather_list + weather
+        context = {
+            'tokyo_today_w':weather_list[4],
+        }
+        return render(request, 'home.html', context)
 
-def main(request):
-    # context = {
-    #     'tokyo_today_w':'晴れ'
-    # }
-    url = 'https://weather.yahoo.co.jp/weather/jp/3.html?day=1' #気象庁のHP
-    res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    soup = BeautifulSoup(res.text, "html.parser")
-    weathers = soup.find_all(class_='weather')
-    date_list = weather_list = []
-    for i  in weathers:
-        weather = re.findall('alt="(.*?)" src', str(i))
-        weather_list = weather_list + weather
-    context = {
-        'tokyo_today_w':weather_list[4],
-    }
-    return render(request, 'home.html', context)
