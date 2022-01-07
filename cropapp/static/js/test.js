a928ec23fac223a2383ac4103645424e412a511d
yuuki330 <katsuyu107@gmail.com>
2022-01-07 10:56:39 +0900

    rotate

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -7,8 +7,6 @@ const medias = {
     }
   };
   const video = document.getElementById("video");
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
   const promise = navigator.mediaDevices.getUserMedia(medias);
   
   promise.then(successCallback)
@@ -16,18 +14,8 @@ const medias = {
   
   function successCallback(stream) {
     video.srcObject = stream;
-    requestAnimationFrame(draw);
   }
   
   function errorCallback(err) {
-    console.log(err);
     alert(err);
-  }
-  
-  function draw() {
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(video, 0, 0);
-  
-    requestAnimationFrame(draw);
   }
