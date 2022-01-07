9a1a068228fc82ae4d7fd69df7862fb3828ec6a9
yuuki330 <katsuyu107@gmail.com>
2022-01-07 19:04:40 +0900

    scraping

--- cropapp/urls.py
+++ cropapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from.views import IndexView, AboutView, TestView, WebcamView
+from.views import IndexView, AboutView, TestView, WebcamView, HomeView
 
 urlpatterns = [
-    path('', WebcamView.as_view()),
+    path('', HomeView.as_view()),
     path('about/', AboutView.as_view()),
 ]
--- cropapp/views.py
+++ cropapp/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+import requests, re
+from bs4 import BeautifulSoup
 
 class IndexView(TemplateView):
     template_name = "cropapp/index.html"
@@ -11,3 +14,25 @@ class TestView(TemplateView):
 
 class WebcamView(TemplateView):
     template_name = "cropapp/webcam.html"
+
+class HomeView(TemplateView):
+    template_name = "cropapp/home.html"
+
+def main(request):
+    url = 'https://www.jma.go.jp/jp/yoho/329.html' #気象庁のHP
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, "html.parser")
+    weathers = soup.find_all(class_='weather')
+    date_list = weather_list = []
+    for i  in weathers:
+        j = str(i).replace('\n','')
+        date = re.findall('<th class="weather">(.*?)<br/>', str(j))
+        date_list = date_list + date
+        weather = re.findall('alt="(.*?)" src', str(j))
+        weather_list = weather_list + weather
+    context = {
+        'seibu_today':date_list[0], 'seibu_tomorrow':date_list[1],
+        'seibu_today_w':weather_list[0], 'seibu_tomorrow_w':weather_list[1],
+        }
+    return render(request, 'main.html', context)
