eccd7e040abd66aff2f2d7ed89dcc0dcb0ef9130
yuuki330 <katsuyu107@gmail.com>
2022-01-10 15:44:40 +0900

    change staticfiles

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -24,7 +24,10 @@
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
+var w = $('.contents').width();
+var h = $('.contents').height();
+$('#canvas').attr('width', w);
+$('#canvas').attr('height', h);
 // const startStreamingVideo = () => {
 //   const video = document.getElementById("video");
 //   if( navigator.mediaDevices.getUserMedia ){
