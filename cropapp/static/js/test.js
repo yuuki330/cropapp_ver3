0c589b44b48b39acb246d5f2edf62f18fb4b7537
yuuki330 <katsuyu107@gmail.com>
2022-01-10 13:12:22 +0900

    camera_test

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -1,21 +1,33 @@
-const medias = {
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment"
-      }
-    }
-  };
-  const video = document.getElementById("video");
-  const promise = navigator.mediaDevices.getUserMedia(medias);
+// const medias = {
+//     audio: false,
+//     video: {
+//       facingMode: {
+//         exact: "environment"
+//       }
+//     }
+//   };
+//   const video = document.getElementById("video");
+//   const promise = navigator.mediaDevices.getUserMedia(medias);
   
-  promise.then(successCallback)
-         .catch(errorCallback);
+//   promise.then(successCallback)
+//          .catch(errorCallback);
   
-  function successCallback(stream) {
-    video.srcObject = stream;
+//   function successCallback(stream) {
+//     video.srcObject = stream;
+//   }
+  
+//   function errorCallback(err) {
+//     alert(err);
+//   }
+
+// On Streaming
+const startStreamingVideo = () => {
+  const video = document.querySelector( "#video" );
+  if( navigator.mediaDevices.getUserMedia ){
+    navigator.mediaDevices.getUserMedia( { video: true } )
+    .then( ( stream ) => {
+        video.srcObject = stream;
+    } );
   }
-  
-  function errorCallback(err) {
-    alert(err);
-  }
+}
+startStreamingVideo();
