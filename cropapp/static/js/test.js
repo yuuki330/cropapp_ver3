7f2379ed254f232d13b938e9b1992d864d55a191
yuuki330 <katsuyu107@gmail.com>
2022-01-10 16:04:18 +0900

    change staticfiles

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -64,6 +64,9 @@ navigator.mediaDevices.getUserMedia( { video: {
 // }
 
 function draw() {
+  video = createCapture(video);
+  video.size(width, height);
+  video.hide();
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
   ctx.drawImage(video, 0, 0);
