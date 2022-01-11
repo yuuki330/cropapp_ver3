b51d252aa103547f55f2ddf009149a3ba923d92d
yuuki330 <katsuyu107@gmail.com>
2022-01-11 14:07:12 +0900

    change webcam

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -47,28 +47,28 @@ const drawVertices = (vertices) => {
 };
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  var video = document.getElementById('video');
-    var constraints = {
-        audio: false,
-        video: {
-            // スマホのバックカメラを使用
-            facingMode: 'environment'
-        }
-    };
-    //  カメラの映像を取得
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then((stream) => {
-            video.srcObject = stream;
-        })
-        .catch((err) => {
-            window.alert(err.name + ': ' + err.message);
-        });
-  video = createCapture(video);
-  video.size(width, height);
-  video.hide();
+createCanvas(windowWidth, windowHeight);
+var video = document.getElementById('video');
+var constraints = {
+      audio: false,
+      video: {
+          // スマホのバックカメラを使用
+          facingMode: 'environment'
+      }
+  };
+  //  カメラの映像を取得
+  navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+          video.srcObject = stream;
+      })
+      .catch((err) => {
+          window.alert(err.name + ': ' + err.message);
+      });
+video = createCapture(video);
+video.size(width, height);
+// video.hide();
 
-  faceapi = ml5.faceApi(video, detectionOptions, onModelReady);
+faceapi = ml5.faceApi(video, detectionOptions, onModelReady);
 }
 
 function draw() {
