6299f9b9793db297783edb819e6a23bb30e512ff
yuuki330 <katsuyu107@gmail.com>
2022-01-11 12:44:42 +0900

    change webcam

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -48,7 +48,23 @@ const drawVertices = (vertices) => {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  video = createCapture(VIDEO);
+  var video = document.getElementById('video');
+    var constraints = {
+        audio: false,
+        video: {
+            // スマホのバックカメラを使用
+            facingMode: 'environment'
+        }
+    };
+    //  カメラの映像を取得
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((err) => {
+            window.alert(err.name + ': ' + err.message);
+        });
+  video = createCapture(video);
   video.size(width, height);
   video.hide();
 
