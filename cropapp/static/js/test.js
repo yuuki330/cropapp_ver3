0680643611da676a0b44805a18eb4c533d290dc4
yuuki330 <katsuyu107@gmail.com>
2022-01-12 12:49:44 +0900

    tensowflow.js_custom

--- cropapp/static/js/test.js
+++ cropapp/static/js/test.js
@@ -262,73 +262,183 @@ This example uses a callback pattern to create the classifier
 //   }
 // }
 
-window.onload = () => {
-  var container = document.getElementById("container");
-  var video  = document.getElementById("camera");
-  video.style.maxWidth = container.clientWidth + "px";
-  video.style.maxHeight = container.clientHeight + "px";
-  var canvas, camera_canvas = null;
-  var context, camera_context = null;
+import React from "react";
+import ReactDOM from "react-dom";
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import "../css/test.css";
+tf.setBackend('webgl');
 
-  const constraints = {
-      audio: false,
-      video: { facingMode: { exact: "environment" }
-      }
+const threshold = 0.75;
+
+async function load_model() {
+    // It's possible to load the model locally or from a repo
+    // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
+    //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+    return model;
+  }
+
+let classesDir = {
+    1: {
+        name: 'Kangaroo',
+        id: 1,
+    },
+    2: {
+        name: 'Other',
+        id: 2,
+    }
+}
+
+class App extends React.Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+
+  componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user"
+          }
+        })
+        .then(stream => {
+          window.stream = stream;
+          this.videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+      const modelPromise = load_model();
+
+      Promise.all([modelPromise, webCamPromise])
+        .then(values => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+    detectFrame = (video, model) => {
+        tf.engine().startScope();
+        model.executeAsync(this.process_input(video)).then(predictions => {
+        this.renderPredictions(predictions, video);
+        requestAnimationFrame(() => {
+          this.detectFrame(video, model);
+        });
+        tf.engine().endScope();
+      });
   };
 
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then( (stream) => {
-      video.srcObject = stream;
-      video.onloadedmetadata = (e) => {
-      video.play();
-      // canvas
-      canvas = document.getElementById("canvas");
-      canvas.style.width = video.clientWidth + "px";
-      canvas.style.height = video.clientHeight + "px";
-      context = canvas.getContext("2d");
-      camera_canvas = document.getElementById("camera_canvas");
-      camera_canvas.style.width = video.clientWidth + "px";
-      camera_canvas.style.height = video.clientHeight + "px";
-      camera_context = camera_canvas.getContext("2d");
-      };
-  })
-  .catch( (err) => {
-      console.log(err.name + ": " + err.message);
-  });
+  process_input(video_frame){
+    const tfimg = tf.browser.fromPixels(video_frame).toInt();
+    const expandedimg = tfimg.transpose([0,1,2]).expandDims();
+    return expandedimg;
+  };
 
-  cocoSsd.load().then(model => {
-      setInterval(function(){detect(model, video, canvas, context)}, 250);
-  });
-  
-  const detect = (model, video, canvas, context) => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      camera_context.clearRect(0, 0, camera_canvas.width, camera_canvas.height);
-      // video.pause();
-      camera_context.drawImage(video, 0, 0, camera_canvas.width, camera_canvas.height);
-      model.detect(camera_canvas).then(res => {
-          // video.play();
-          if(res.length == 0) return;
-          for (var i = 0; i < res.length; i++) {
-              var score = parseInt(res[i]["score"] * 100 ,10);
-              drawRect(context, res[i].bbox[0], res[i].bbox[1], res[i].bbox[2], res[i].bbox[3], score)
-              drawName(context, res[i]["class"], res[i].bbox[0], res[i].bbox[1], score);
-          }
-      });
+  buildDetectedObjects(scores, threshold, boxes, classes, classesDir) {
+    const detectionObjects = []
+    var video_frame = document.getElementById('frame');
+
+    scores[0].forEach((score, i) => {
+      if (score > threshold) {
+        const bbox = [];
+        const minY = boxes[0][i][0] * video_frame.offsetHeight;
+        const minX = boxes[0][i][1] * video_frame.offsetWidth;
+        const maxY = boxes[0][i][2] * video_frame.offsetHeight;
+        const maxX = boxes[0][i][3] * video_frame.offsetWidth;
+        bbox[0] = minX;
+        bbox[1] = minY;
+        bbox[2] = maxX - minX;
+        bbox[3] = maxY - minY;
+        detectionObjects.push({
+          class: classes[i],
+          label: classesDir[classes[i]].name,
+          score: score.toFixed(4),
+          bbox: bbox
+        })
+      }
+    })
+    return detectionObjects
   }
-};
-function drawRect(ctx, x, y, w, h, score) {
-  ctx.beginPath();
-  ctx.rect(parseInt(x, 10), parseInt(y, 10), parseInt(w, 10), parseInt(h, 10));
-  ctx.lineWidth = 7.5;
-  ctx.strokeStyle =  score < 75 ? "rgb(255, 255, 0)" : "rgb(50, 240, 60)";
-  ctx.stroke();
-  ctx.closePath();
+
+  renderPredictions = predictions => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    //Getting predictions
+    const boxes = predictions[4].arraySync();
+    const scores = predictions[5].arraySync();
+    const classes = predictions[6].dataSync();
+    const detections = this.buildDetectedObjects(scores, threshold,
+                                    boxes, classes, classesDir);
+
+    detections.forEach(item => {
+      const x = item['bbox'][0];
+      const y = item['bbox'][1];
+      const width = item['bbox'][2];
+      const height = item['bbox'][3];
+
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(item["label"] + " " + (100 * item["score"]).toFixed(2) + "%").width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    detections.forEach(item => {
+      const x = item['bbox'][0];
+      const y = item['bbox'][1];
+
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(item["label"] + " " + (100*item["score"]).toFixed(2) + "%", x, y);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Real-Time Object Detection: Kangaroo</h1>
+        <h3>MobileNetV2</h3>
+        <video
+          style={{height: '600px', width: "500px"}}
+          className="size"
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="600"
+          height="500"
+          id="frame"
+        />
+        <canvas
+          className="size"
+          ref={this.canvasRef}
+          width="600"
+          height="500"
+        />
+      </div>
+    );
+  }
 }
 
-function drawName(ctx, text, x, y, score) {
-  ctx.beginPath();
-  ctx.fillText(text + " : " + score + "%", parseInt(x, 10), parseInt(y, 10));
-  ctx.fillStyle = "red";
-  ctx.font = "10px serif";
-  ctx.closePath();
-}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
