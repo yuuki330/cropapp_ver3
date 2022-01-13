da0c6ba77e88d222d3b00a523c62575c408e1ce7
yuuki330 <katsuyu107@gmail.com>
2022-01-13 13:26:57 +0900

    tomato

--- cropapp/static/js/predict.js
+++ cropapp/static/js/predict.js
@@ -79,7 +79,7 @@ async function predict(){
   // const zeros = tf.zeros([1, 640, 640, 3]);
 	// let prediction = await model.predict(tensor).data();
   let prediction = await model.executeAsync(tensor);
-  console.log(prediction[0].strides);
+  console.log(prediction[2].dataSync());
 	let results = Array.from(prediction)
 				.map(function(p,i){
 	return {
