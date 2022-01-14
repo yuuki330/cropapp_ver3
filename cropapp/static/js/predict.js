ab490d8ddb91cf7986594ebc2a3d812d8a9bb651
yuuki330 <katsuyu107@gmail.com>
2022-01-14 17:00:07 +0900

    tomato

--- cropapp/static/js/predict.js
+++ cropapp/static/js/predict.js
@@ -38,6 +38,9 @@ async function loadModel() {
 //-----------------------
 
 var video;
+var canvas    = document.createElement("canvas");
+var ctx   = canvas.getContext('2d');
+
 function startWebcam() {
 	console.log("video streaming start.");
 	$("#console").html(`<li>video streaming start.</li>`);
@@ -370,8 +373,8 @@ function bbox_iou(box1, box2){
 //------------------------------
 
 function captureWebcam() {
-	var canvas    = document.createElement("canvas");
-	var ctx   = canvas.getContext('2d');
+	// var canvas    = document.createElement("canvas");
+	// var ctx   = canvas.getContext('2d');
 	canvas.width  = video.width;
 	canvas.height = video.height;
 
