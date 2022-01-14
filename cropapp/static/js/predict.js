a4df6e63ba5f00ed80b0618e8d792debb0d692ee
yuuki330 <katsuyu107@gmail.com>
2022-01-14 19:08:01 +0900

    tomato

--- cropapp/static/js/predict.js
+++ cropapp/static/js/predict.js
@@ -69,7 +69,7 @@ function startWebcam() {
 
 $("#predict-button").click(function(){
 	// setInterval(predict, 1000/10);
-  setInterval(predict, 500);
+  setInterval(predict, 200);
 });
 
 //-----------------------
