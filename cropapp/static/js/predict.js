0006509566dd4c7af71ef6cc7438febd6fc26c4f
yuuki330 <katsuyu107@gmail.com>
2022-01-21 12:41:36 +0900

    web UI

--- cropapp/static/js/predict.js
+++ cropapp/static/js/predict.js
@@ -2,12 +2,12 @@
 var MODEL_HEIGHT = 1;
 var MODEL_WIDTH = 1;
 
+document.addEventListener('DOMContentLoaded');
+
 var video = document.getElementById("main-stream-video");
 // const resolution = { w: 1080, h: 720 };
 // var canvas_hide = document.getElementById("main-stream-canvas");
 // var ctx = canvas.getContext('2d');
-
-document.addEventListener('DOMContentLoaded');
 var new_canvas = document.createElement("canvas");
 new_canvas.setAttribute('id', 'canvas_1')
 var canvas = document.getElementById("canvas_1");
