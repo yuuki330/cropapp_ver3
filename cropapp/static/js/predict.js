f45ec9257be64185895b9e3c23bc26ac16441f61
yuuki330 <katsuyu107@gmail.com>
2022-01-21 12:14:46 +0900

    web UI

--- cropapp/static/js/predict.js
+++ cropapp/static/js/predict.js
@@ -40,7 +40,7 @@ $("#camera1").click(function(){
   setInterval(() => {
     if (canvas && ctx){
         // ctx.drawImage(video, 0, 0, video.width, video.height);
-        ctx.drawImage(video, 0, 0, window.innerWidth, window.innerHeight);
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         // ctx.scale(1,1);
     }
   }, 1000/30);
