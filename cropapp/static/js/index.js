835076a051db5e3f7777f63b978a2dbdcb06cad9
yuuki330 <katsuyu107@gmail.com>
2022-01-24 09:13:52 +0900

    web UI

--- cropapp/static/js/index.js
+++ cropapp/static/js/index.js
@@ -1,4 +1,5 @@
 let net;
+const webcamElement = document.getElementById('webcam');
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -7,10 +8,24 @@ async function app() {
   net = await mobilenet.load();
   console.log('Successfully loaded model');
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
 }
 
 app();
