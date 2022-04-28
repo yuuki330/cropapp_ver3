f51faa5204f0bfa17b280acce2ab76399f6dcf3c
yuuki330 <katsuyu107@gmail.com>
2022-04-28 17:30:56 +0900

    4/28 Adding home_9

--- cropapp/urls.py
+++ cropapp/urls.py
@@ -4,6 +4,6 @@ from.views import IndexView, HomeView
 urlpatterns = [
     # path('', home, name = 'home'),
     path('', HomeView.as_view()),
-    path('index/', HomeView.as_view(), name="index"),
+    path('index/', IndexView.as_view(), name="index"),
     # path('webcam/', webcam, name = 'webcam'),
 ]
