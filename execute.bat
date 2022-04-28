f0b4519fcc6aa98ed23ab89554789084d0a8eda5
yuuki330 <katsuyu107@gmail.com>
2022-04-28 14:57:02 +0900

    4/28 Changeing save_text CSS_9

--- execute.bat
+++ execute.bat
@@ -1,5 +1,5 @@
 python manage.py collectstatic
 git add .
-git commit -m "4/28 Changeing save_text CSS_8"
+git commit -m "4/28 Changeing save_text CSS_9"
 git push origin master
 git push heroku master
