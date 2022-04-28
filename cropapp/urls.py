b1fceab54ac9cc41abce68bbd48b997e7cd99c37
yuuki330 <katsuyu107@gmail.com>
2022-04-28 16:49:10 +0900

    4/28 Adding home_3

--- cropapp/urls.py
+++ cropapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from.views import IndexView, HomeView, views
+from.views import IndexView, HomeView, Index
 
 urlpatterns = [
     # path('', home, name = 'home'),
     path('', HomeView.as_view()),
-    path('index/', views.Index, name='index'),
+    path('index/', Index, name='index'),
     # path('webcam/', webcam, name = 'webcam'),
 ]
