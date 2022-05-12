00c702b4d5f7e6e0d8a86f23e70bd06384295a3c
yuuki330 <katsuyu107@gmail.com>
2022-05-12 16:55:14 +0900

    5/12 Adding title ver3

--- cropapp/static/js/index.js
+++ cropapp/static/js/index.js
@@ -12,6 +12,29 @@ var webcamElement = document.getElementById('webcam');
 //var deviceid;
 const WEBCAM_CONFIG = {facingMode: "environment"};
 
+$(function () {
+  var webStorage = function () {
+    if (sessionStorage.getItem('access')) {
+      /*
+        2回目以降アクセス時の処理
+      */
+      $(".loading").addClass('is-active');
+    } else {
+      /*
+        初回アクセス時の処理
+      */
+      sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
+      $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
+      setTimeout(function () {
+        // ローディングを数秒後に非表示にする
+        $(".loading").addClass('is-active');
+        $(".loading-animation").removeClass('is-active');
+      }, 3000); // ローディングを表示する時間
+    }
+  }
+  webStorage();
+});
+
 async function app() {
   console.log('Loading mobilenet..');
 
