049bb5f41593ea460bf876483a52d5da3191eee4
yuuki330 <katsuyu107@gmail.com>
2022-05-12 15:51:42 +0900

    5/12 Adding title

--- cropapp/urls.py
+++ cropapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from.views import IndexView, HomeView
+from.views import IndexView
 
 urlpatterns = [
-    # path('', HomeView.as_view()),
     path('', IndexView.as_view()),
     # path('index/', IndexView.as_view(), name="index"),
 ]
