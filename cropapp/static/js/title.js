4662f120515632f1b50ae81bc6ce234345a8f031
yuuki330 <katsuyu107@gmail.com>
2022-05-26 18:44:58 +0900

    5/26 蜀咏悄蟾ｮ縺玲崛縺・繧ｹ繧ｯ繝ｭ繝ｼ繝ｫ遖∵ｭ｢

--- cropapp/static/js/title.js
+++ cropapp/static/js/title.js
@@ -1,8 +1,29 @@
+function scroll_control(event) {
+    event.preventDefault();
+}
+
+function no_scroll() {
+    // PCでのスクロール禁止
+    document.addEventListener("mousewheel", scroll_control, { passive: false });
+    // スマホでのタッチ操作でのスクロール禁止
+    document.addEventListener("touchmove", scroll_control, { passive: false });
+}
+
+// スクロール禁止解除
+function return_scroll() {
+    // PCでのスクロール禁止解除
+    document.removeEventListener("mousewheel", scroll_control, { passive: false });
+    // スマホでのタッチ操作でのスクロール禁止解除
+    document.removeEventListener('touchmove', scroll_control, { passive: false });
+}
+
 $(function() {
     setTimeout(function(){
+        no_scroll();
         $('.start p').fadeIn(3200);
     }, 500);  // 0.5秒後にロゴをフェードイン！
     setTimeout(function(){
         $('.start').fadeOut(1000);
+        return_scroll();
     }, 3000);  // 3.0秒後にロゴ含め真っ白背景をフェードアウト！
 });
