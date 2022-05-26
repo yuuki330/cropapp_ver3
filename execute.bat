98248b38c9f409458668a86aeb288f964d4c553e
yuuki330 <katsuyu107@gmail.com>
2022-05-26 16:47:26 +0900

    5/26 繧ｹ繧ｯ繝ｭ繝ｼ繝ｫ蝠城｡・height200 fadeout 4.0s

--- execute.bat
+++ execute.bat
@@ -1,5 +1,5 @@
 python manage.py collectstatic
 git add .
-git commit -m "5/26 スクロール問題 height1000%"
+git commit -m "5/26 スクロール問題 height200% fadeout 4.0s"
 git push origin master
 git push heroku master
